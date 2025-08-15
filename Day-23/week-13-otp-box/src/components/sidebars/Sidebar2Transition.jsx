6863a94880b2ed2ba39959442fab7dd49cbd9613
Sanjit Dutta <110589185+sanjitxdutta@users.noreply.github.com>
2025-08-15 17:06:22 +0530

    Delete Day-23/week-13-otp-box directory

--- Day-23/week-13-otp-box/src/components/sidebars/Sidebar2Transition.jsx
+++
@@ -1,6 +0,0 @@
-
-export const Sidebar2Transition = () => {
-    return <div>
-        <button className="transition-all duration-1000 bg-red-200 hover:bg-green-200 p-4 hover:p-8">hi there</button>
-    </div>
-}
